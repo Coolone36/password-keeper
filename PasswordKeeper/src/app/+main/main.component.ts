34785596496679c6e6d52e9963eb83ec102e20cf
Coolone36 <michaeljlaritz@gmail.com>
2018-04-16 01:20:39 -0400

    Added automatic signout, has copied code in 3 locations, will eventually refactor. Used the router to navigate.

--- PasswordKeeper/src/app/+main/main.component.ts
+++ PasswordKeeper/src/app/+main/main.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, OnDestroy {
 
-  constructor() { }
 
-  ngOnInit() {
+  private authStateSubscription: Subscription;
+
+  ngOnDestroy(): void {
+    this.authStateSubscription.unsubscribe();
+  }
+  ngOnInit(): void {
+    this.authStateSubscription = this.afAuth.authState.subscribe((user: firebase.User)=> {
+      if(user){
+        //Sign in just happened
+        console.log("User is signed in: " + user.uid);
+      } else {
+        // signout occurred
+        console.log("User is not signed in");
+        this.router.navigate(["/signin"]);
+      }
+    })
+  }
+
+  constructor(private afAuth: AngularFireAuth, private router: Router) {
   }
 
 }
--- PasswordKeeper/src/app/+signin/signin.component.ts
+++ PasswordKeeper/src/app/+signin/signin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth'
 import * as firebase from 'firebase/app'
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-signin',
@@ -9,18 +11,36 @@ import * as firebase from 'firebase/app'
 })
 export class SigninComponent implements OnInit {
 
-  constructor(public afAuth: AngularFireAuth) {}
-
-  ngOnInit() {
-  }
+  constructor(private afAuth: AngularFireAuth, private router: Router) {}
 
   login() {
     this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
-    
+    // Navigate to naked domain
+    // this.router.navigate(["/"]);
   }
 
   signInWithRoseFire(): void{
     console.log("TODO: Signin with rose fire")
   }
+
+  private authStateSubscription: Subscription;
+
+  ngOnDestroy(): void {
+    this.authStateSubscription.unsubscribe();
+  }
+  ngOnInit(): void {
+    this.authStateSubscription = this.afAuth.authState.subscribe((user: firebase.User)=> {
+      if(user){
+        //Sign in just happened
+        console.log("User is signed in.");
+        this.router.navigate(["/"]);
+      } else {
+        // signout occurred
+        console.log("User is not signed in");
+        
+      }
+    })
+  }
+
 
 }
