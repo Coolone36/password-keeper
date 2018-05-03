0f413a2fa1a7b45041d80b31655cb932fdcb2163
Coolone36 <michaeljlaritz@gmail.com>
2018-05-03 01:45:48 -0400

    created dialog content w/o firebase connection

--- PasswordKeeper/src/app/password-dialog/password-dialog.component.ts
+++ PasswordKeeper/src/app/password-dialog/password-dialog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Password } from '../models/password.model';
+import { MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-password-dialog',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordDialogComponent implements OnInit {
 
-  constructor() { }
+  formPassword: Password;
+
+  constructor(private dialogRef: MatDialogRef<PasswordDialogComponent>) {
+    this.formPassword = new Password();
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    console.log("Submit and save form: ", this.formPassword);
+    
+    this.dialogRef.close();
   }
 
 }
